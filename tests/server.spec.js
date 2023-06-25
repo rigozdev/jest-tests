1238be3a8b590f2cf4dfa6c77e4f0dddb87fec61
Rodrigo Gonzalez <rodrigo.gonzaleztr@outlook.com>
2023-06-25 03:10:06 -0400

    Se agregan test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -11,7 +11,6 @@ describe("Operaciones CRUD de cafes", () => {
     test('Obteniendo respuesta de GET /cafes un tipo de dato arreglo', async () => {
         const { body } = await request(server).get('/cafes').send();
         const cafe = body;
-        //console.log(cafe);
         expect(cafe).toBeInstanceOf(Array);// Comprueba si es una instancia de Array/Arreglo
     })
     test('Obteniendo respuesta de GET /cafes no vacío', async () => {
@@ -22,29 +21,22 @@ describe("Operaciones CRUD de cafes", () => {
     });
     test("Eliminando un cafe", async () => {
         const jwt = "Authorization";
-        const idDeCafeAEliminar = 1;
-        const { body: cafes } = await request(server).delete(`/cafes/${idDeCafeAEliminar}`).set("Authorization", jwt).send();
-        const ids = await cafes.map(p => p.id);
-        expect(ids).not.toContain(idDeCafeAEliminar);
-    });
-    test("Eliminando un cafe", async () => {
-        const jwt = "Authorization";
-        const idDeCafeAEliminar = 5;
+        const idDeCafeAEliminar = 5; //Si cambio 'idDeCafeAEliminar' a un id que si esté contenido en arreglo fallará el test
         const { status } = await request(server).delete(`/cafes/${idDeCafeAEliminar}`).set("Authorization", jwt).send();
-        expect(status).toBe(404);
+        expect(status).toBe(404);//Espero el error 404
     });
     test("Agregando un nuevo cafe", async () => {
-        const nuevoCafe = { id: 5, nombre: "Cafe Test"};
-        const { body: cafes, statusCode } = await request(server).post("/cafes").send(nuevoCafe);    
-        expect(statusCode).toBe(201);           
-        expect(cafes).toContainEqual(nuevoCafe); //Espera que el nuevo café 
+        const nuevoCafe = { id: 5, nombre: "Cafe Test" };
+        const { body: cafes, status } = await request(server).post("/cafes").send(nuevoCafe);
+        expect(status).toBe(201);
+        expect(cafes).toContainEqual(nuevoCafe); //Espera que el nuevoCafe no esté contenido dentro del arreglo cafes
     });
     test("Actualizando un café con id incorrecto devuelve status code 400", async () => {
-        const idParametro = 1;
+        const idParametro = 1;//Creo parametro y payload con distinto valor
         const idPayload = 2;
-        const cafeActualizado = { id: idPayload, nombre: "Cafe Actualizado" };    
-        const { statusCode } = await request(server).put(`/cafes/${idParametro}`).send(cafeActualizado);
-    
-        expect(statusCode).toBe(400);
+        const cafeActualizado = { id: idPayload, nombre: "Cafe Actualizado" };//paso valor de payload 
+        const { status } = await request(server).put(`/cafes/${idParametro}`).send(cafeActualizado);//paso valor de parametro distinto a payload
+        //Se fuerza el error 400 (que no coinciden payload y parametro)
+        expect(status).toBe(400);//Espero el error 400
     });
 });
